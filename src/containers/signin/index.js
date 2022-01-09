e036763796324c630387bab22b9c10e88a61fdc8
abdulkhaliquem9 <abdulkhaliquem9@gmail.com>
2022-01-10 02:51:04 +0530

    added signup and signin pages

--- src/containers/signin/index.js
+++ src/containers/signin/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Alert, Form, Button, Container, PageHeader } from 'react-bootstrap'
+import './index.css'
+
 
 function Signin() {
     return (
-        <div>
-            /signin
+        <div className="sign-up">
+            <Container>
+                <Form>
+                    <Alert variant="danger">
+                        <strong>Wrong User Name or Password </strong>
+                    </Alert>
+                    <h3>Sign In</h3>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Sign In
+                    </Button>
+                </Form>
+            </Container>
         </div>
     )
 }
--- src/containers/signup/index.js
+++ src/containers/signup/index.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Form, Button, Container, PageHeader } from 'react-bootstrap'
+import './index.css'
 
 function Signup() {
+    const [initialState, setState] = useState({email:"", displayName:"", password:"", confirmPassword:""})
+    //Using higher order function
+    const onChangeHandler = (key) => (event) => {
+        console.log(`${key} change`, event.target.value)
+        setState({...initialState, [key]: event.target.value}) //{key: }
+    }
     return (
-        <div>
-            
-            Signup
+        <div className="sign-up">
+        <Container>
+            <Form>
+            <h3>Create Account</h3>
+                <Form.Group className="mb-3" controlId="formBasicEmail" >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" onChange={event => onChangeHandler("email")(event)}/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicDisplayName">
+                    <Form.Label>Display Name</Form.Label>
+                    <Form.Control type="text" placeholder="User Name"  onChange={onChangeHandler("displayName")} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password"  onChange={onChangeHandler("password")}/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password"  onChange={onChangeHandler("confirmPassowrd")}/>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    SignUp
+                </Button>
+            </Form>
+        </Container>
         </div>
     )
 }
